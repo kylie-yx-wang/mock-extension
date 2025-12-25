d4ceb2dcd767e73002f29d94b81b516e98a9dc4e
kylie-yx-wang <yuxinwa2@andrew.cmu.edu>
2025-12-24 20:45:06 -0500

    Update pop.js

--- pop.js
+++ pop.js
@@ -1,1 +1,17 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const input = document.getElementById("textbox");
+  const result = document.getElementById("result");
 
+  input.addEventListener("input", (e) => {
+    const text = e.target.value;
+    let newValue = "";
+
+    for (let i = 0; i < text.length; i++) {
+      newValue += (i % 2)
+        ? text[i].toUpperCase()
+        : text[i];
+    }
+
+    result.textContent = newValue;
+  });
+});
