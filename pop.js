97b20b958ad5db56490ffa6c99e20d4789c52d24
kylie-yx-wang <yuxinwa2@andrew.cmu.edu>
2025-12-25 18:44:57 -0500

    Update copy button

--- pop.js
+++ pop.js
@@ -1,7 +1,8 @@
 document.addEventListener("DOMContentLoaded", () => {
   const input = document.getElementById("textbox");
   const result = document.getElementById("result");
-
+  const copyBtn = document.getElementById("copyBtn");
+  
   input.addEventListener("input", (e) => {
     const text = e.target.value;
     let newValue = "";
@@ -14,4 +15,19 @@ document.addEventListener("DOMContentLoaded", () => {
 
     result.textContent = newValue;
   });
+  
+  copyBtn.addEventListener("click", async () => {
+    const copiedText = result.textContent;
+  
+    if (!copiedText) return;
+  
+    try {
+      await navigator.clipboard.writeText(copiedText);
+      copyBtn.textContent = "Copied!";
+      setTimeout(() => copyBtn.textContent = "Copy", 1000);
+    } catch (err) {
+      console.error(err);
+    }
+  });
+
 });
